f88253657480346c42197ebdacfbfa16842aafa4
Seqquid <ilya.mindiyarov@list.ru>
2025-05-14 13:35:05 +0300

    Адаптировал под мобилки, скорретировал визуально страницу проекта и главную страницу.

--- src/pages/ProjectDetails.js
+++ src/pages/ProjectDetails.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../styles/ProjectDetails.css';
+import img2 from '../images/img2.jpg';
 
 const ProjectDetails = () => {
   return (
@@ -7,6 +8,9 @@ const ProjectDetails = () => {
       <h2>Детали проекта</h2>
       <div className="project-info">
         <h3>Название проекта</h3>
+        <div className='project-item'>
+          <img src={img2}  className='project-image' alt={"img2"}/>
+        </div>
         <p><strong>Категория:</strong> Разработка</p>
         <p><strong>Бюджет:</strong> 25 000₽</p>
         <p><strong>Описание:</strong> Нужно разработать веб-приложение с авторизацией, профилями и API.</p>
